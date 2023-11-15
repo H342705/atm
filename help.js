1aba1aac1663e0e12a7b8cb40c84a2f71c6f53ed
Hamza Rahim <136451582+H342705@users.noreply.github.com>
2023-11-15 20:46:31 +0500

    Add files via upload

---
+++ help.js
@@ -0,0 +1,61 @@
+import gradient from "gradient-string";
+export let login = [
+    {
+        type: "number",
+        name: "data",
+        message: gradient.rainbow("Please Enter your Pin to login"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please Enter Your Pin in Numbers";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+];
+export let atmFunction = [
+    {
+        type: "list",
+        name: "option",
+        choices: ["Deposit Money", "withdraw Money", "Check Balance"],
+        message: gradient.rainbow("Please select the service you want")
+    }
+];
+export let deposit = [
+    {
+        type: "number",
+        name: "moneydeposit",
+        message: gradient.rainbow("Enter your Deposit Money"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please Enter Your Amount in Numbers";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+];
+export let withDraw = [
+    {
+        type: "number",
+        name: "moneyWithdraw",
+        message: gradient.rainbow("Enter amount to withdraw from your account"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please Enter Your Amount in Numbers";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+];
+export let confirmed = [
+    {
+        type: "confirm",
+        name: "more",
+        message: gradient.morning("Do You want another Transaction")
+    }
+];
---
+++ index.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import showBanner from "node-banner";
+import chalk from "chalk";
+import {login,atmFunction,deposit,withDraw,confirmed} from "./help.js"
+(async () => {
+    await showBanner('Hamza ATM', 'Your Account Pin & Balance will generate randomly','blue','green');
+})();
+/***************************generate random data *******************************/
+let randomPin: number = Math.round(Math.random()*10000);    
+interface data {
+    userPin: number,
+    accountbalance: number
+}
+let randomdata: data = {
+    userPin: randomPin,
+    accountbalance: 1000
+}
+/***************************Main function of the Code ****************************/
+let condition: boolean = true;
+setTimeout(async () => {
+    console.log(chalk.bold.magentaBright.italic(`Your Account Pin is ${randomdata.userPin} and balance is ${randomdata.accountbalance}`));
+    let {data} = await inquirer.prompt(login);                                                     // Asking User for Pin
+    if (data === randomdata.userPin){                                                              // Comparing userPin with random Pin
+        console.log(gradient.pastel("Congratulation, You are login to Hamza ATM services"));
+        do {                                                                                       // create loops for user further transaction
+            let {option} = await inquirer.prompt(atmFunction);                                     // Showing Atm functionality to user 
+            if(option === 'Deposit Money'){                                                        // when user want to deposit money
+                let  {moneydeposit} = await inquirer.prompt(deposit);                              // Asking for amount user want to deposit
+                randomdata.accountbalance += moneydeposit;                                         // depoist money is added to user account
+                console.log(gradient.mind("Congratulation, Your cash is deposit successfully"));
+                console.log(gradient.vice("Your new Account Balance is " + randomdata.accountbalance));
+                let {more} = await inquirer.prompt(confirmed);                                      // Asking user for further transaction
+                condition = more;                                                                   // Saving user response in loop condition 
+            }else if(option === 'Check Balance'){                                                   // when user want to check balance
+                console.log(gradient.mind(`Your current Account Balance is  ${randomdata.accountbalance}`));        
+                let {more} = await inquirer.prompt(confirmed);                                      // Asking user for further transaction
+                condition = more;                                                                   // Saving user response in loop condition 
+            }else if(option === 'withdraw Money'){                                                  // when user want to withdraw money
+                let {moneyWithdraw} = await inquirer.prompt(withDraw);                              // Asking for amount user want to withdraw
+                if(moneyWithdraw > randomdata.accountbalance){                                      // checking if withdraw amount is more than
+                    console.log(gradient.retro("Sorry, You have Insufficient Balance to withdraw"));                // available balance
+                    let {more} = await inquirer.prompt(confirmed);                                  // Asking user for further transaction
+                    condition = more;                                                               // Saving user response in loop condition
+                }else {                                                                             
+                    randomdata.accountbalance -= moneyWithdraw;                                     // withdraw money is remove from user accnt
+                    console.log(gradient.mind("Congratulation, Your cash is withdraw successfully"));
+                    console.log(gradient.vice("Your new Account Balance is "+ randomdata.accountbalance));
+                    let {more} = await inquirer.prompt(confirmed);                                  // Asking user for further transaction
+                    condition = more;                                                               // Saving user response in loop condition
+                }
+            }
+        }while(condition)                                                                           // loop condition for further execution
+        }else {
+            console.log(chalk.bold.redBright.italic("Sorry, Your Pin is invalid"));                        // if user enter invalid pin
+    }
+}, 1000);                                                                                           // functiom execute after 1 second
